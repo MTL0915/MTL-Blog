a22deb6492fb05d97d09da72526883e91a170975
Matianle0915 <18923236683@163.com>
2021-08-17 18:38:46 +0800

    'swiper'

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,49 +15,66 @@
       </div>
     </div>
     <div class="banner">
-      <swiper ref="mySwiper" :options="swiperOptions">
-        <swiper-slide>Slide 1</swiper-slide>
-        <swiper-slide>Slide 2</swiper-slide>
-        <swiper-slide>Slide 3</swiper-slide>
-        <swiper-slide>Slide 4</swiper-slide>
-        <swiper-slide>Slide 5</swiper-slide>
+      <swiper :options="swiperOption" ref="mySwiper">
+        <swiper-slide v-for="(item, index) in bannerImgs" :key="index">
+          <img :src="item" alt="" />
+        </swiper-slide>
         <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
       </swiper>
     </div>
   </div>
 </template>
 
 <script>
-import { Swiper, SwiperSlide, directive } from "vue-awesome-swiper";
-// import style (>= Swiper 6.x)
-import 'swiper/swiper-bundle.css'
+// 引入插件
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
 
 export default {
-  name: "Header",
-  data () {
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data() {
     return {
-      banners: [ '/1.jpg', '/2.jpg', '/3.jpg' ],
+      bannerImgs:[
+        "https://img.zcool.cn/ad_manager/location/e48d611b139d1101c43350ae6b31.jpg",
+        "https://img.zcool.cn/ad_manager/location/7439611b137f1101c43350934225.png",
+        "https://img.zcool.cn/ad_manager/location/27f5611b14041101c433502dc500.jpg"
+      ],
       swiperOption: {
-        pagination: {
-          el: '.swiper-pagination'
+        loop: true,
+        autoplay: {
+          delay: 3000,
+          stopOnLastSlide: false,
+          disableOnInteraction: false
         },
+        // 显示分页
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true //允许分页点击跳转
+        },
+        // 设置点击箭头
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
       }
+    };
+  },
+  computed: {
+    swiper() {
+      return this.$refs.mySwiper.swiper;
     }
   },
   mounted() {
-    console.log('Current Swiper instance object', this.mySwiper)
-    this.mySwiper.slideTo(3, 1000, false)
-  },
-  props: {
-      msg: String,
-  },
-  components: {
-      Swiper,
-      SwiperSlide,
-  },
-  directives: {
-    swiper: directive,
-  },
+    // current swiper instance
+    // 然后你就可以使用当前上下文内的swiper对象去做你想做的事了
+    console.log("this is current swiper instance object", this.swiper);
+    // this.swiper.slideTo(3, 1000, false);
+  }
 };
 </script>
 
@@ -86,5 +103,8 @@ export default {
 }
 .menu ul li a {
   padding-left: 40px;
+}
+.banner img{
+  width:100%;
 }
 </style>
