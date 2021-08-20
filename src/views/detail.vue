ed567882b3b0b18b090138d0d74dd92831d5edab
Matianle0915 <18923236683@163.com>
2021-08-20 18:03:05 +0800

    'detail'

---
+++ src/views/detail.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="detail">
+  <Header></Header>
+  <div class="content">
+    <ArticleDetailBox class="fl"></ArticleDetailBox>
+    <Category class="fr"></Category>
+  </div>
+  <Footer></Footer>
+</div>
+
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import ArticleDetailBox from '../components/ArticleDetailBox.vue'
+import Category from '../components/Category.vue'
+import Footer from '../components/Footer.vue'
+export default {
+  components: {
+    Header,
+    ArticleDetailBox,
+    Category,
+    Footer
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.content{
+  width:1200px;
+  margin: 0 auto;
+  overflow: hidden;
+  margin-top:50px;
+}
+.fl{
+  float: left;
+}
+.fr{
+  float: right;
+}
+</style>
--- src/views/works.vue
+++ src/views/works.vue
@@ -2,16 +2,119 @@
   <div class="works">
     <Header></Header>
     <div class="works-content">
-      <svg class="icon" aria-hidden="true">
-        <use xlink:href="#icondiannao"></use>
-      </svg>
+      <div class="terminal">
+        <div class="terminal-tag">
+          <i class="iconfont icondiannao"></i>
+          <span class="terminal-title">WEB</span>
+        </div>
+        <div class="terminal-tag">
+          <i class="iconfont iconshouji"></i>
+          <span class="terminal-title">APP</span>
+        </div>
+        <div class="terminal-tag">
+          <i class="iconfont iconxiaochengxu"></i>
+          <span class="terminal-title">WECHAT</span>
+        </div>
+      </div>
+      <div class="myWorks">
+        <ul>
+          <li class="list-item">
+            <a href="#">
+              <img
+                src="https://puui.qpic.cn/qqvideo_ori/0/v0039pm6h21_360_204/0"
+              />
+            </a>
+            <div class="intro">
+              <a href="">百老汇经典音乐剧《国王与我》上线</a>
+              <div class="createTime">2021/8/21</div>
+            </div>
+          </li>
+          <li class="list-item">
+            <a href="#">
+              <img
+                src="https://puui.qpic.cn/qqvideo_ori/0/v0039pm6h21_360_204/0"
+              />
+            </a>
+            <div class="intro">
+              <a href="">百老汇经典音乐剧《国王与我》上线</a>
+              <div class="createTime">2021/8/21</div>
+            </div>
+          </li>
+          <li class="list-item">
+            <a href="#">
+              <img
+                src="https://puui.qpic.cn/qqvideo_ori/0/v0039pm6h21_360_204/0"
+              />
+            </a>
+            <div class="intro">
+              <a href="">百老汇经典音乐剧《国王与我》上线</a>
+              <div class="createTime">2021/8/21</div>
+            </div>
+          </li>
+          <li class="list-item">
+            <a href="#">
+              <img
+                src="https://puui.qpic.cn/qqvideo_ori/0/v0039pm6h21_360_204/0"
+              />
+            </a>
+            <div class="intro">
+              <a href="">百老汇经典音乐剧《国王与我》上线</a>
+              <div class="createTime">2021/8/21</div>
+            </div>
+          </li>
+          <li class="list-item">
+            <a href="#">
+              <img
+                src="https://puui.qpic.cn/qqvideo_ori/0/v0039pm6h21_360_204/0"
+              />
+            </a>
+            <div class="intro">
+              <a href="">百老汇经典音乐剧《国王与我》上线</a>
+              <div class="createTime">2021/8/21</div>
+            </div>
+          </li>
+          <li class="list-item">
+            <a href="#">
+              <img
+                src="https://puui.qpic.cn/qqvideo_ori/0/v0039pm6h21_360_204/0"
+              />
+            </a>
+            <div class="intro">
+              <a href="">百老汇经典音乐剧《国王与我》上线</a>
+              <div class="createTime">2021/8/21</div>
+            </div>
+          </li>
+          <li class="list-item">
+            <a href="#">
+              <img
+                src="https://puui.qpic.cn/qqvideo_ori/0/v0039pm6h21_360_204/0"
+              />
+            </a>
+            <div class="intro">
+              <a href="">百老汇经典音乐剧《国王与我》上线</a>
+              <div class="createTime">2021/8/21</div>
+            </div>
+          </li>
+          <li class="list-item">
+            <a href="#">
+              <img
+                src="https://puui.qpic.cn/qqvideo_ori/0/v0039pm6h21_360_204/0"
+              />
+            </a>
+            <div class="intro">
+              <a href="">百老汇经典音乐剧《国王与我》上线</a>
+              <div class="createTime">2021/8/21</div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
-import "../assets/js/iconfont";
+import "../assets/css/iconfont.css";
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
 export default {
@@ -26,11 +129,53 @@ export default {
 </script>
 
 <style scoped>
-.icon {
-  width: 1em;
-  height: 1em;
-  vertical-align: -0.15em;
-  fill: currentColor;
-  overflow: hidden;
+.works-content {
+  width: 1200px;
+  margin: 0 auto;
+  margin-top: 30px;
+}
+.iconfont {
+  font-size: 30px;
+  padding-right: 5px;
+}
+.terminal {
+  display: flex;
+  justify-content: center;
+  padding-bottom: 20px;
+  border-bottom: solid 1px #abcdef;
+}
+.terminal-tag {
+  display: flex;
+  align-items: center;
+  padding: 0 40px;
+}
+.myWorks {
+  margin-top: 30px;
+  margin-right: -20px;
+}
+.myWorks .list-item {
+  width: 285px;
+  vertical-align: top;
+  display: inline-block;
+  margin-right: 20px;
+  margin-bottom:30px;
+}
+.myWorks .list-item img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 4px;
+  background: #f8f8f8;
+}
+.intro {
+  text-align: center;
+  margin-top:10px;
+}
+.intro a {
+  color: #444444;
+}
+.intro .createTime{
+  margin-top:5px;
 }
 </style>
